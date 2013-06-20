5ab7f51674e12af221adeeefffc147b6042701d9
Clément Doumouro <clement.doumouro@gmail.com>
2013-06-20 18:15:08 +0200

    Fini avec l'identification et déconnexion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,6 @@ module ApplicationHelper
   end
   
   def logo
-      image_tag("logo.png", :alt => "Application exemple", :class => "round")
+    image_tag("logo.png", :alt => "Application Exemple", :class => "round")
   end
 end
