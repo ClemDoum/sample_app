b6ed5b5ab299d96f9722f5d53113f891db4c5ced
Clément Doumouro <clement.doumouro@gmail.com>
2013-06-26 16:01:20 +0200

    ajout du suivi des utilisateurs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8
 
-
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:show, :new, :create] 
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
   
@@ -61,6 +60,19 @@ class UsersController < ApplicationController
     @titre = @user.nom
   end
   
+  def following
+    @titre = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @titre = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
   
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 require 'digest'
+
 class User < ActiveRecord::Base
+
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :nom, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  
+  has_many :relationships, :foreign_key => "follower_id",
+  :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+  :class_name => "Relationship",
+  :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+  
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -12,6 +24,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,:format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
 
   validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
+
 
   before_save :encrypt_password
   
@@ -38,8 +51,19 @@ class User < ActiveRecord::Base
   end
   
   def feed
-      # C'est un préliminaire. Cf. chapitre 12 pour l'implémentation complète.
-      Micropost.where("user_id = ?", id)
+     Micropost.from_users_followed_by(self)
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
 
